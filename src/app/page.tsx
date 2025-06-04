db100c9178fdfb9c5ce21be5354c7961a62d2f35
Abdullah Al Jubair <jubair2810@gmail.com>
2025-06-04 22:42:51 +0600

    added:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-// pages/video/[id].tsx
-
-import HLSPlayer from "./component/HLSPlayer";
+"use client";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
 
 const VideoPage = () => {
   const videoId = "237172d0-e323-49d6-8c17-cc1aaeccf97d"; // normally from URL or fetched
   const hlsUrl = `https://pub-cba03f5ad4224f06a69192a6a26ac2b4.r2.dev/videos/f5925fa2-169e-4236-8330-0e9777a36fb0/master.m3u8`;
 
   return (
-    <div className="max-w-3xl mx-auto mt-10">
-      <HLSPlayer src={hlsUrl} />
-    </div>
+    <MediaPlayer
+      title="My Video"
+      src={hlsUrl}
+      crossOrigin="anonymous"
+      onQualitiesChange={}
+      controls
+      fullscreenOrientation="landscape"
+    >
+      <MediaProvider />
+      <DefaultVideoLayout icons={defaultLayoutIcons} />
+    </MediaPlayer>
   );
 };
 
